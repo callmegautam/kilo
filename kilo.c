b41cfa99a50b213d99fbf839bfdd491971c064ba
Gautam Suthar <iamgautamsuthar@outlook.com>
2026-01-15 09:09:15 +0530

    add: raw mode for user input in terminal

--- kilo.c
+++ kilo.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-  printf("Hello From Kilo the Editor");
+  enableRawMode();
   return EXIT_SUCCESS;
 }
